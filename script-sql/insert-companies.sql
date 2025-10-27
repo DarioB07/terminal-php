fd7d5ac141be3d1bae27d0f52e01804a52788a87
dario <darioburbanohernandez@gmail.com>
2025-10-26 19:55:50 -0500

    Add Insert Companies Script

---
+++ script-sql/insert-companies.sql
@@ -0,0 +1,7 @@
+INSERT INTO terminal.company
+(id, name, email, phone, web)
+values
+(0, 'Cootranar', 'cootranar@gmail.com', '57 988627263', 'www.cootranar.com'),
+(0, 'Transipiales', 'transipiales@gmail.com', '57 988627263', 'www.transipiales.com'),
+(0, 'Gacela', 'gacela@gmail.com', '57 988627263', 'www.gacela.com'),
+(0, 'Bolivariano', 'bolivariano@gmail.com', '57 988627263', 'www.bolivariano.com');
