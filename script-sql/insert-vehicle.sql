9329316ca69dbe9f58d2721f383b7b59ef0ba587
dario <darioburbanohernandez@gmail.com>
2025-10-26 22:24:56 -0500

    Add Insert Vehicle.sql

---
+++ script-sql/insert-vehicle.sql
@@ -0,0 +1,20 @@
+INSERT INTO terminal.vehicle (id, `type`, capacity, plaque, company_id)
+VALUES
+-- Cootranar
+(1, 'buseta', 20, 'ABC123', 1),
+(2, 'bus', 40, 'DEF456', 1),
+(3, 'aerovan', 15, 'GHI789', 1),
+
+-- Transipiales
+(4, 'buseta', 20, 'JKL321', 2),
+(5, 'bus', 40, 'MNO654', 2),
+(6, 'aerovan', 15, 'PQR987', 2),
+
+-- Gacela
+(7, 'buseta', 20, 'STU111', 3),
+(8, 'bus', 40, 'VWX222', 3),
+(9, 'aerovan', 15, 'YZA333', 3),
+
+-- Bolivariano
+(11, 'bus', 40, 'EFG555', 4),
+(12, 'aerovan', 15, 'HIJ666', 4);
